460f15a1478cb2398202d8bc2c8e6672554ebbb1
Matysys <matysys333@outlook.com>
2023-03-29 17:34:13 -0300

    Concluindo o projeto da DIO

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,59 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+class Usuario(val nome: String)
 
-class Usuario
+data class ConteudoEducacional(val nome: List<String>, val duracao: Int = 60)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>) {
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+    fun mostrarFormacao(){
+        println("Nome da formação: $nome")
+
+        println("Conteúdo da formação:");
+        conteudos.forEach{ conteudo -> conteudo.nome.forEach{ nomeConteudo ->
+            println("- $nomeConteudo")
+        }
+        }
+    }
 
     val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+
+    fun matricular(vararg usuario: Usuario) {
+        for(aluno in usuario) {
+            inscritos.add(aluno)
+            //println(aluno.nome)
+        }
+    }
+
+
+    fun mostrarInscritos(){
+        val total = inscritos.size
+        val nivel = Nivel.INTERMEDIARIO
+        val mensagem = when(nivel){
+            Nivel.BASICO -> "avançado"
+            Nivel.INTERMEDIARIO -> "intermediário"
+            Nivel.AVANCADO -> "avançado"
+        }
+
+        println("Número de inscritos: $total")
+        println("Nível da formação: $mensagem")
+        println("Alunos inscritos: ")
+        for(aluno in inscritos) {
+            println("- ${aluno.nome}")
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val aluno1 = Usuario("Mateus")
+    val aluno2 = Usuario("Pedro")
+    val aluno3 = Usuario("Paula")
+
+    val conteudo = ConteudoEducacional(listOf("HTTP", "API REST", "Bibliotecas", "Funções"), 120)
+    val formacao = Formacao("Backend com Kotlin", listOf(conteudo))
+    formacao.matricular(aluno1, aluno2, aluno3)
+    formacao.mostrarFormacao()
+    formacao.mostrarInscritos()
+
+
 }
