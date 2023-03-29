6aba73812b73746070f406bf6dcc54b56e9889f0
Matysys <matysys333@outlook.com>
2023-03-29 17:44:21 -0300

    Colocando a duração do curso nas informações.

--- desafio.kt
+++ desafio.kt
@@ -2,13 +2,13 @@ enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
 class Usuario(val nome: String)
 
-data class ConteudoEducacional(val nome: List<String>, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: List<String>, val duracao: Int)
 
-data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>, val duracao: Int) {
 
     fun mostrarFormacao(){
         println("Nome da formação: $nome")
-
+        println("Duração da formação: $duracao horas")
         println("Conteúdo da formação:");
         conteudos.forEach{ conteudo -> conteudo.nome.forEach{ nomeConteudo ->
             println("- $nomeConteudo")
@@ -19,6 +19,7 @@ data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>) 
     val inscritos = mutableListOf<Usuario>()
 
     fun matricular(vararg usuario: Usuario) {
+        //TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
         for(aluno in usuario) {
             inscritos.add(aluno)
             //println(aluno.nome)
@@ -50,7 +51,7 @@ fun main() {
     val aluno3 = Usuario("Paula")
 
     val conteudo = ConteudoEducacional(listOf("HTTP", "API REST", "Bibliotecas", "Funções"), 120)
-    val formacao = Formacao("Backend com Kotlin", listOf(conteudo))
+    val formacao = Formacao("Backend com Kotlin", listOf(conteudo), conteudo.duracao)
     formacao.matricular(aluno1, aluno2, aluno3)
     formacao.mostrarFormacao()
     formacao.mostrarInscritos()
